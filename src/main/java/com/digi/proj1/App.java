9c4f67537aac56c1c856985a53d94867f738bfdc
saiakhil46 <sqaiakhil46@gmail.com>
2019-07-16 10:39:30 +0000

    hello world commit

--- src/main/java/com/digi/proj1/App.java
+++ src/main/java/com/digi/proj1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello this is Java standalone app" );
+        System.out.println( "Hello world" );
     }
 }
